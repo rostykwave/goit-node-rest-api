9eea01388d501d90d12fc5fef2bbca4db0a5be0a
Rostyslav <rostykwave@gmail.com>
2024-07-28 15:52:21 +0300

    chore: contactsService from previous task

---
+++ services/contactsServices.js
@@ -0,0 +1,35 @@
+import { promises as fs } from "fs";
+import path from "path";
+import { nanoid } from "nanoid";
+
+const contactsPath = path.resolve("./db/contacts.json");
+
+async function listContacts() {
+  const data = await fs.readFile(contactsPath, "utf-8");
+  return JSON.parse(data);
+}
+
+async function getContactById(contactId) {
+  const contacts = await listContacts();
+  const contact = contacts.find((contact) => contact.id === contactId);
+  return contact || null;
+}
+
+async function removeContact(contactId) {
+  const contacts = await listContacts();
+  const index = contacts.findIndex((contact) => contact.id === contactId);
+  if (index === -1) return null;
+  const [removedContact] = contacts.splice(index, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return removedContact;
+}
+
+async function addContact(name, email, phone) {
+  const contacts = await listContacts();
+  const newContact = { id: nanoid(), name, email, phone };
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return newContact;
+}
+
+export { listContacts, getContactById, removeContact, addContact };
