13978a80d78c4ab85eefa616ab84fd5bf56722ea
Rostyslav <rostykwave@gmail.com>
2024-08-27 16:32:59 +0300

    feat: add sendEmail and verificationToken check

--- services/authServices.js
+++ services/authServices.js
@@ -1,6 +1,9 @@
 import bcrypt from "bcrypt";
 import User from "../db/models/User.js";
 import gravatar from "gravatar";
+import { nanoid } from "nanoid";
+import sendEmail from "../helpers/sendEmail.js";
+const { BASE_URL } = process.env;
 
 export const findUser = (query) =>
   User.findOne({
@@ -17,16 +20,31 @@ export const updateUser = async (query, data) => {
   });
 };
 
+export const sendVerifyEmail = (email, verificationToken) => {
+  const verifyEmail = {
+    to: email,
+    subject: "Verify email",
+    html: `<a target="_blank" href="${BASE_URL}/api/auth/verify/${verificationToken}">Click to verify email</a>`,
+  };
+
+  return sendEmail(verifyEmail);
+};
+
 export const register = async (data) => {
   try {
     const { password } = data;
     const hashPassword = await bcrypt.hash(password, 10);
     const avatarURL = gravatar.url(data.email);
+    const verificationToken = nanoid();
     const newUser = await User.create({
       ...data,
       password: hashPassword,
       avatarURL,
+      verificationToken,
     });
+
+    await sendVerifyEmail(data.email, verificationToken);
+
     return newUser;
   } catch (error) {
     if (error?.parent?.code === "23505") {
